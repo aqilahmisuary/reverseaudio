50d790b5d102697e4e3065f755f95aedabfe34cd
Aqilah Misuary <aqilah.misuary@bandlab.com>
2018-08-08 17:34:37 +0800

    Fixed audio file not loading error

--- javascripts/main.js
+++ javascripts/main.js
@@ -9,7 +9,7 @@ if (contextClass) {
 };
 
 var request = new XMLHttpRequest();
-request.open('GET', "https://dl.dropboxusercontent.com/u/30075450/Always%20In%20My%20Head%20-%20Coldplay.mp3", true);
+request.open('GET', "https://upload.wikimedia.org/wikipedia/commons/c/c8/Example.ogg", true);
 request.responseType = 'arraybuffer';
 request.onload = function() {
 context.decodeAudioData(request.response, function(arrayBuffer) {
@@ -37,7 +37,7 @@ analyser.fftSize = 2048;
 var bufferLength = analyser.frequencyBinCount;
 var dataArray = new Uint8Array(bufferLength);
 
-analyser.getByteTimeDomainData(dataArray); 
+analyser.getByteTimeDomainData(dataArray);
 myCanvas.clearRect(0, 0, WIDTH, HEIGHT);
 
 canvas.width = 1000;
@@ -46,18 +46,18 @@ function draw() {
 
   drawVisual = requestAnimationFrame(draw);
   analyser.getByteTimeDomainData(dataArray);
-  
+
   myCanvas.fillStyle = '255,255,255, 0.1';
   myCanvas.fillRect(0, 0, WIDTH, HEIGHT);
   myCanvas.lineWidth = 2;
   myCanvas.strokeStyle = 'rgb(255,255,255)';
   myCanvas.beginPath();
-  
+
   var sliceWidth = WIDTH * 1.0 / bufferLength;
   var x = 0;
-  
+
   for(var i = 0; i < bufferLength; i++) {
-   
+
         var v = dataArray[i] / 128.0;
         var y = v * HEIGHT/2;
 
@@ -69,36 +69,36 @@ function draw() {
 
         x += sliceWidth;
       };
-  
+
     myCanvas.lineTo(canvas.width, canvas.height/2);
     myCanvas.stroke();
     };
 
 
 playButton.addEventListener("mousedown", function() {
-  
+
   var source = context.createBufferSource();
   var now = context.currentTime;
   var mixGain = context.createGain();
-    
+
   source.buffer = buffer;
   var soundDuration = source.buffer.duration;
   Array.prototype.reverse.call(buffer.getChannelData(0));
   Array.prototype.reverse.call(buffer.getChannelData(1));
-  
+
   source.connect(analyser);
   analyser.connect(context.destination);
-  
+
   source.start(now);
   draw();
-    
+
   stopButton.addEventListener("mousedown", function() {
-  
+
   mixGain.gain.linearRampToValueAtTime(0, now + 0.2);
   source.stop(now + 0.2);
-  
+
  }, false);
-  
+
  }, false);
 
 
